6e29c31fee1dd90dd0e02e3baebc6a04ed3d29b1
dmitriibb616@gmail.com <dmitriibb616@gmail.com>
2022-04-26 19:33:13 +0300

    first commit

---
+++ src/test/java/com/dmbb/testflow/TestflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmbb.testflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
